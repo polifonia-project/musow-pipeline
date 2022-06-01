10fb4d77bb4c546101e0b57e9c922b64434740ac
Laurent Fintoni <72798408+laurentfintoni@users.noreply.github.com>
2022-06-01 09:30:04 +0200

    updates

--- NOTEBOOKS/PYTHON_FILES/LogReg_Searches.py
+++ NOTEBOOKS/PYTHON_FILES/LogReg_Searches.py
@@ -1,9 +1,9 @@
-from pipeline_functions import *
+from PYTHON_FILES.pipeline_functions import *
 
 class LogRegSearches(object):
 
-    def train(t_input, t_feature, target, cv_int, score_type, filename, path):
-        return lr_training(t_input, t_feature, target, cv_int, score_type, filename, path)
+    def train(t_input, t_feature, target, cv_int, score_type, max_feats, stopwords, filename, path):
+        return lr_training(t_input, t_feature, target, cv_int, score_type, max_feats, stopwords, filename, path)
 
     def predict(path, filename, p_input, p_feature):
         return lr_predict(path, filename, p_input, p_feature)
@@ -11,8 +11,8 @@ class LogRegSearches(object):
     def search_twitter(token, keyword, start, end, mresults, mcount, file_name):
         return twitter_search(token, keyword, start, end, mresults, mcount, file_name)
 
-    def scrape_links(link_list):
-        return scrape_links(link_list)
+    def scrape_links(link_list, pred_df, filename):
+        return scrape_links(link_list, pred_df, filename)
     
     def search_twitter_weekly(token, keyword_list, max_results, max_counts):
         return twitter_search_weekly (token, keyword_list, max_results, max_counts)
@@ -20,8 +20,8 @@ class LogRegSearches(object):
     def search_twitter_custom (token, keyword_list, start_list, end_list, max_results, max_counts):
         return twitter_search_custom (token, keyword_list, start_list, end_list, max_results, max_counts)
 
-    def tweets_to_classify(path, filetype):
-        return tweets_to_classify(path, filetype)
+    def tweets_to_classify(folder, filetype):
+        return tweets_to_classify(folder, filetype)
     
     def predict_twitter(path, filename, p_input, p_feature, score, filter):
         return twitter_predictions(path, filename, p_input, p_feature, score, filter)
