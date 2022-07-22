72a31d7bc4ddbcc5e4ad8382b87ec298bf9bd1d7
Laurent Fintoni <72798408+laurentfintoni@users.noreply.github.com>
2022-07-22 15:22:02 +0200

    Revert "v3 updates"

--- NOTEBOOKS/musow_pipeline/text_prediction.py
+++
@@ -1,9 +0,0 @@
-from musow_pipeline.text_functions import *
-
-class TextPrediction(object):
-
-    def scrape_links(link_list, pred_df, filename):
-        return scrape_links(link_list, pred_df, filename)
-    
-    def resource_predictions(path, filename, p_input, p_feature, score, savefile):
-        return resource_predictions(path, filename, p_input, p_feature, score, savefile)
